c84ad26b83c4986d25d3360cd0d0d47138eda3ef
rg3915 <rg3915@yahoo.com.br>
2020-04-12 21:07:57 -0300

    Add Project

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="#092e20"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+        <span><i class="fa fa-youtube-play fa-2x"></i></span>
+        <h2 class="ml-5">Regis do Python</h2>
+
+      </div>
+
+      <v-row
+        align="center"
+        class="ml-10"
+        style="max-width: 650px"
+      >
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          outlined
+          append-icon="mdi-magnify"
+          color="white"
+          hide-details
+        />
+      </v-row>
+
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
